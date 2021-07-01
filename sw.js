8b2556ca50d3b384a41331bc9868f1b03675fe51
Saurer1994 <markussaurer@gmx.at>
2021-07-01 07:21:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener("install", e => {
 })
 
 self.addEventListener("fetch", e => {
-    e.responseWith(
+    e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
